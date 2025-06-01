57dbb52aa9cff1147de7a92eac70284a0da1c70c
AndrewYuan34 <yiboy@uvic.ca>
2025-06-01 12:41:48 -0400

    feat: Sets up the basic structure for the camera system, including basic camera class files, test framework, and CMake configuration updates.

---
+++ tests/CameraTest.cpp
@@ -0,0 +1,21 @@
+#include <gtest/gtest.h>
+#include "Utils/Camera.h"
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+namespace Boids {
+namespace Tests {
+
+class CameraTest : public ::testing::Test {
+protected:
+    void SetUp() override {
+        // Setup code that will be called before each test
+    }
+
+    void TearDown() override {
+        // Cleanup code that will be called after each test
+    }
+};
+
+} // namespace Tests
+} // namespace Boids 
