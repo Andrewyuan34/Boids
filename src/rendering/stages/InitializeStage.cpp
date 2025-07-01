aa16b76c98db4cd2a9c78377e956a33519ab25c6
AndrewYuan34 <yiboy@uvic.ca>
2025-06-30 23:56:55 -0400

    Add additional macro check for Linux compilation

--- src/rendering/stages/InitializeStage.cpp
+++ src/rendering/stages/InitializeStage.cpp
@@ -1,8 +1,17 @@
 #include "InitializeStage.h"
 #include "../Pipeline.h"
 #include <iostream>
+#include <cassert>
 #include <bgfx/bgfx.h>
 #include <bgfx/embedded_shader.h>
+
+// Disable DirectX shaders on Linux (following openblack's approach)
+#if BX_PLATFORM_LINUX
+#undef BGFX_EMBEDDED_SHADER_DXBC
+#define BGFX_EMBEDDED_SHADER_DXBC(...)
+#undef BGFX_EMBEDDED_SHADER_DX9BC
+#define BGFX_EMBEDDED_SHADER_DX9BC(...)
+#endif
 
 // Include generated shader headers
 #define SHADER_NAME vs_simple
@@ -40,18 +49,19 @@ void InitializeStage::OnDetach() {
 }
 
 void InitializeStage::SetupBGFX() {
-    // Create shader program
+    // Create shader program using embedded shaders
     bgfx::RendererType::Enum type = bgfx::getRendererType();
 
     // Create vertex shader
     bgfx::ShaderHandle vsh = bgfx::createEmbeddedShader(s_embeddedShaders, type, "vs_simple");
+    assert(bgfx::isValid(vsh));
 
     // Create fragment shader
     bgfx::ShaderHandle fsh = bgfx::createEmbeddedShader(s_embeddedShaders, type, "fs_simple");
+    assert(bgfx::isValid(fsh));
 
     // Create shader program
     m_Program = bgfx::createProgram(vsh, fsh, true);
-
     std::cout << "InitializeStage: BGFX shader setup completed" << std::endl;
 }
 
